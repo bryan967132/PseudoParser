808977d13fa3c20549f3bc445aa26ad568ba9dac
bryan967132 <bryan.tejaxun@gmail.com>
2023-11-14 13:09:03 -0600

    Rediseño Interfaz

--- src/Interface/IconFile.java
+++ src/Interface/IconFile.java
@@ -4,7 +4,6 @@ import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
-import java.util.Map;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
