a5e30333cdebca0bd7fd2e785ef50354676a29fa
bryan967132 <bryan.tejaxun@gmail.com>
2023-11-17 14:24:28 -0600

    Métodos Generadores de Código

--- src/Classes/Abstracts/Expression.java
+++ src/Classes/Abstracts/Expression.java
@@ -1,13 +1,19 @@
 package Classes.Abstracts;
 import Classes.Env.Env;
+import Classes.Generators.GoGen;
+import Classes.Generators.PyGen;
 import Classes.Utils.ReturnType;
 import Classes.Utils.TypeExp;
 import Classes.Utils.TypeSent;
 public abstract class Expression extends Sentence {
+    public boolean isGrp = false;
     public TypeExp typeExp;
+    public boolean isInstruction = false;
     public Expression(int line, int column, TypeExp typeExp) {
         super(line, column, TypeSent.EXPRESSION);
         this.typeExp = typeExp;
     }
     public abstract ReturnType exec(Env env);
+    public abstract ReturnType goGenerate(Env env, GoGen goGen);
+    public abstract ReturnType pyGenerate(Env env, PyGen pyGen);
 }
--- src/Classes/Abstracts/Instruction.java
+++ src/Classes/Abstracts/Instruction.java
@@ -1,5 +1,7 @@
 package Classes.Abstracts;
 import Classes.Env.Env;
+import Classes.Generators.GoGen;
+import Classes.Generators.PyGen;
 import Classes.Utils.TypeInst;
 import Classes.Utils.TypeSent;
 public abstract class Instruction extends Sentence {
@@ -9,4 +11,6 @@ public abstract class Instruction extends Sentence {
         this.typeInst = typeInst;
     }
     public abstract void exec(Env env);
+    public abstract void goGenerate(Env env, GoGen goGen);
+    public abstract void pyGenerate(Env env, PyGen pyGen);
 }
