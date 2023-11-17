6ecdfb87e611b7742e3426c8c2e0b0b5c0347745
bryan967132 <bryan.tejaxun@gmail.com>
2023-11-17 14:28:47 -0600

    Controlador

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -14,6 +14,9 @@ import javax.swing.JTextPane;
 import javax.swing.text.StyledDocument;
 import Classes.Abstracts.Instruction;
 import Classes.Env.Env;
+import Classes.Generators.GoGen;
+import Classes.Generators.PyGen;
+import Classes.Instructions.Function;
 import Classes.Instructions.MainMethod;
 import Classes.Utils.Outs;
 import Classes.Utils.TypeInst;
@@ -41,26 +44,26 @@ public class Controller {
     public void setFormat(JTextPane editor) {
         try {
             StyledDocument doc = editor.getStyledDocument();
-            String input = doc.getText(0,doc.getLength());
+            String input = doc.getText(0, doc.getLength());
             WordPainter painter = new WordPainter();
             ScannerF scanner = new ScannerF(
                 new BufferedReader(
                     new StringReader(input)
-                ),
+                ), 
                 painter
             );
             painter.setStyle(editor);
-            ParserF parser = new ParserF(scanner,painter);
+            ParserF parser = new ParserF(scanner, painter);
             parser.parse();
         }
         catch(Exception e) {}
     }
-    public void analyze(IDE ide, int index, JTextPane editor, JTextPane console) {
+    public void analyze(IDE ide,  int index,  JTextPane editor,  JTextPane console) {
         IconFile currentFile = pjs.get(index);
+        Outs.resetOuts();
         try {
-            Outs.resetOuts();
             StyledDocument doc = editor.getStyledDocument();
-            String input = doc.getText(0,doc.getLength());
+            String input = doc.getText(0, doc.getLength());
             Scanner scanner = new Scanner(
                 new BufferedReader(
                     new StringReader(input)
@@ -68,8 +71,7 @@ public class Controller {
             );
             Parser parser = new Parser(scanner);
             parser.parse();
-            Classes.Utils.Outs.printConsole = new ArrayList<>();
-            Env global = new Env(null, "Global");
+            Env global = new Env(null,  "Global");
             MainMethod mainMethod = null;
             for (Instruction instruction : parser.execute) {
                 try {
@@ -92,17 +94,107 @@ public class Controller {
             console.setText(outPrint);
         }
     }
-    public void saveOLCPJ(int index,JTextPane editor) {
+    public void generateGoCode(IDE ide,  int index,  JTextPane editor,  JTextPane console) {
+        IconFile currentFile = pjs.get(index);
+        Outs.resetOuts();
+        try {
+            StyledDocument doc = editor.getStyledDocument();
+            String input = doc.getText(0, doc.getLength());
+            Scanner scanner = new Scanner(
+                new BufferedReader(
+                    new StringReader(input)
+                )
+            );
+            Parser parser = new Parser(scanner);
+            parser.parse();
+            Env global = new Env(null,  "Global");
+            GoGen goGen = new GoGen();
+            for (Instruction instruction : parser.execute) {
+                try {
+                    if (instruction.typeInst != TypeInst.MAIN) {
+                        instruction.exec(global);
+                    }
+                } catch (Exception e) {}
+            }
+            for(Instruction instruction : parser.execute) {
+                instruction.goGenerate(global, goGen);
+            }
+            goGen.generateFinalCode();
+            String outPrint = "PseudoP: " + currentFile.name + "\n";
+            Outs.printConsole.add(0, String.join("\n", goGen.getFinalCode()));
+            outPrint += Outs.getStringOuts();
+            console.setText(outPrint);
+            exportCode(String.join("\n", goGen.getFinalCode()), parser.fileName, parser.fileName + ".go");
+        } catch (Exception e) {
+            String outPrint = "PseudoP: " + currentFile.name + "\n";
+            outPrint += Outs.getStringOuts();
+            console.setText(outPrint);
+        }
+    }
+    public void generatePyCode(IDE ide,  int index,  JTextPane editor,  JTextPane console) {
+        IconFile currentFile = pjs.get(index);
+        Outs.resetOuts();
+        try {
+            StyledDocument doc = editor.getStyledDocument();
+            String input = doc.getText(0, doc.getLength());
+            Scanner scanner = new Scanner(
+                new BufferedReader(
+                    new StringReader(input)
+                )
+            );
+            Parser parser = new Parser(scanner);
+            parser.parse();
+            Env global = new Env(null,  "Global");
+            PyGen pyGen = new PyGen();
+            for (Instruction instruction : parser.execute) {
+                try {
+                    if (instruction.typeInst != TypeInst.MAIN) {
+                        instruction.exec(global);
+                    }
+                } catch (Exception e) {}
+            }
+            for(Instruction instruction : parser.execute) {
+                instruction.pyGenerate(global, pyGen);
+            }
+            pyGen.generateFinalCode();
+            String outPrint = "PseudoP: " + currentFile.name + "\n";
+            Outs.printConsole.add(0, String.join("\n", pyGen.getFinalCode()));
+            outPrint += Outs.getStringOuts();
+            console.setText(outPrint);
+            exportCode(String.join("\n", pyGen.getFinalCode()), parser.fileName, parser.fileName + ".py");
+        } catch (Exception e) {
+            String outPrint = "PseudoP: " + currentFile.name + "\n";
+            outPrint += Outs.getStringOuts();
+            console.setText(outPrint);
+        }
+    }
+    public void savePJ(int index, JTextPane editor) {
         try {
             BufferedWriter writer = new BufferedWriter(
                 new OutputStreamWriter(
-                    new FileOutputStream(pjs.get(index).path),
+                    new FileOutputStream(pjs.get(index).path), 
                     "utf-8"
                 )
             );
             StyledDocument doc = editor.getStyledDocument();
-            String input = doc.getText(0,doc.getLength());
+            String input = doc.getText(0, doc.getLength());
             writer.write(input);
+            writer.close();
+        } catch (Exception e) {}
+    }
+    public void exportCode(String content, String dir, String fileName) {
+        try {
+            File dirName = new File("./Out/" + dir);
+            if(!dirName.exists()) {
+                dirName.mkdirs();
+            }
+            BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                    new FileOutputStream("./Out/" + dir + "/" + fileName), 
+                    "utf-8"
+                )
+            );
+            writer.write(content);
             writer.close();
         } catch (Exception e) {}
     }
@@ -110,7 +202,7 @@ public class Controller {
         try {
             File archivo = new File(path);
             FileInputStream fis = new FileInputStream(archivo);
-            InputStreamReader isr = new InputStreamReader(fis,"UTF-8");
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             String text = "";
             String line;
@@ -131,7 +223,7 @@ public class Controller {
         try {
             Path[] pjs1 = new Path[pjs.size()];
             for(int i = 0; i < pjs.size(); i ++) {
-                pjs1[i] = new Path(pjs.get(i).id,pjs.get(i).path);
+                pjs1[i] = new Path(pjs.get(i).id, pjs.get(i).path);
             }
             FileOutputStream file = new FileOutputStream("bin/Files");
             ObjectOutputStream output = new ObjectOutputStream(file);
@@ -151,7 +243,7 @@ public class Controller {
             pjs = new ArrayList<>();
             int i = 0;
             for(Path path : pjs1) {
-                pjs.add(new IconFile(i,new File(path.path),ide,this));
+                pjs.add(new IconFile(i, new File(path.path), ide, this));
                 i ++;
             }
         }
