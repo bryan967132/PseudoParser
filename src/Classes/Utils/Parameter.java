b1d6fe27184eee45ebd82af39a988342e500353a
bryan967132 <bryan.tejaxun@gmail.com>
2023-11-22 22:31:37 -0600

    Utilidades: Parameter

--- src/Classes/Utils/Parameter.java
+++ src/Classes/Utils/Parameter.java
@@ -1,8 +1,12 @@
 package Classes.Utils;
 public class Parameter {
+    public int line;
+    public int column;
     public String id;
     public Type type;
     public Parameter(int line, int column, String id, Type type) {
+        this.line = line;
+        this.column = column;
         this.id = id;
         this.type = type;
     }
