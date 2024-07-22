91fe78c00917df8641c8fd9d7bba47483be96748
bryan967132 <bryan.tejaxun@gmail.com>
2024-07-22 15:47:43 -0600

    Outs

--- src/Classes/Utils/Outs.java
+++ src/Classes/Utils/Outs.java
@@ -1,28 +1,18 @@
 package Classes.Utils;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 public class Outs {
     public static ArrayList<String> printConsole = new ArrayList<>();
     public static ArrayList<Error> errors = new ArrayList<>();
     public static String getStringOuts() {
-        String out = "";
-        for(int i = 0; i < printConsole.size(); i ++) {
-            out += printConsole.get(i);
-            if(i < printConsole.size() - 1) {
-                out += "\n";
-            }
-        }
+        String out = printConsole.stream().map(Object::toString).collect(Collectors.joining("\n"));
         if(errors.size() > 0) {
             if(!out.equals("")) {
                 out += "\n\n↳ ERRORES\n";
             } else {
                 out += "↳ ERRORES\n";
             }
-            for(int i = 0; i < errors.size(); i ++) {
-                out += errors.get(i);
-                if(i < errors.size() - 1) {
-                    out += "\n";
-                }
-            }
+            out += errors.stream().map(Error::toString).collect(Collectors.joining("\n"));
         }
         return out;
     }
