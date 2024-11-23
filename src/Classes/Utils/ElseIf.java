46d22ca73bd4e374bee2b8112754dbe2d5da4798
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2024-11-23 01:02:42 -0600

    [Update] Utils

--- src/Classes/Utils/ElseIf.java
+++ src/Classes/Utils/ElseIf.java
@@ -1,10 +1,10 @@
 package Classes.Utils;
 import java.util.ArrayList;
 import Classes.Abstracts.Statement;
-public class ElseIfStruct {
+public class ElseIf {
     public ArrayList<Statement> instructions;
     public Statement _if;
-    public ElseIfStruct(ArrayList<Statement> instructions, Statement _if) {
+    public ElseIf(ArrayList<Statement> instructions, Statement _if) {
         this.instructions = instructions;
         this._if = _if;
     }
